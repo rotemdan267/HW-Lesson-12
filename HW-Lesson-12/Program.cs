8f5758bd5b76795251d0b5336d7da0652124a625
rotemdan267 <rotemdan267@gmail.com>
2021-08-04 01:07:09 +0300

    Add project files.

---
+++ HW-Lesson-12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Lesson_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q-6
+
+            DateTime dt = new DateTime();
+            
+            Quotation q = new Quotation("hgh", dt, 7);
+            for (int i = 0; i < q.SumLines.Length; i++)
+            {
+                q.SumLines[i] = i + 1;
+            }
+            double[] arr = q.SumLines;
+            double sum = 0;
+            for (int i = 0; i < q.SumLines.Length; i++)
+            {
+                sum += q.SumLines[i];
+            }
+            for (int i = 0; i <arr.Length; i++)
+            {
+                arr[i] = i + 2;
+            }
+
+
+            q.ChangeMainDetails("dsdss", dt);
+
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HW-Lesson-12/Quotation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Lesson_12
+{
+    class Quotation
+    {
+        public string CustomerName { get; private set; }
+        public double[] SumLines { get; set; }
+        public double DocSum 
+        {
+            get
+            {
+                double sum = 0;
+                for (int i = 0; i < SumLines.Length; i++)
+                {
+                    sum += SumLines[i];
+                }
+                return sum;
+            }
+        }
+        public DateTime ValidUntil { get; private set; }
+
+        public Quotation(string customerName, DateTime validUntil, int numOfLines)
+        {
+            CustomerName = customerName;
+            ValidUntil = validUntil;
+            SumLines = new double[numOfLines];
+        }
+
+        public void ChangeMainDetails(string customerName, DateTime validUntil)
+        {
+            CustomerName = customerName;
+            ValidUntil = validUntil;
+        }
+    }
+}
